b9de331024615fef0129f526eb9ac1c6c22237f0
NguyenHoangPhuc1711 <phucbi1711@gmail.com>
2026-04-06 15:08:31 +0700

    Add files via upload

---
+++ Bài Tập/Baif 1.py
@@ -0,0 +1,4 @@
+import math
+r = int(input("r: "))
+chu_vi_hinh_tron = 2*r*math.pi
+print(f"chu vi hinh tron: {chu_vi_hinh_tron}")
---
+++ Bài Tập/Baif 2.py
@@ -0,0 +1,6 @@
+m = int(input("Nhập m dài vào đây"))
+n = int(input("Nhập n vào đây"))
+tong = m + n
+hieu = m - n
+tich = m * n
+print(tong,hieu, tich)
